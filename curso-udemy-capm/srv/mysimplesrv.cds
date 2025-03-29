3c93a98161814293b00b84e1a69b6c9852e9c9cd
Matias Lovato <matii_seba_11@hotmail.com>
2025-03-29 19:48:23 -0300

    nivel2capm

---
+++ curso-udemy-capm/srv/mysimplesrv.cds
@@ -0,0 +1,15 @@
+using myCompany.hr.lms from '../db/Students';
+
+service mysrvdemo{
+    @readonly entity StudentSRV as projection on lms.Students;
+    @readonly entity StudentsSRV as projection on lms.Students;
+
+    @readonly entity GetStudent as projection on lms.Students;
+    @updateonly entity UpdateStudent as projection on lms.Students;
+    @insertonly entity InsertStudent as projection on lms.Students;
+    @deleteonly entity DeleteStudent as projection on lms.Students;
+
+}
+
+
+
---
+++ curso-udemy-capm/srv/mysimplesrv.js
@@ -0,0 +1,127 @@
+const cds = require("@sap/cds");
+const { Students } = cds.entities("myCompany.hr.lms");
+
+module.exports = srv => {
+  
+
+  if (!Students) {
+    console.error("❌ ERROR: La entidad 'Students' no está definida en el servicio.");
+    return;
+  }
+
+  srv.on("READ", "StudentsSRV", async (req) => {  // Hacemos la función async
+    const result = await cds.run(SELECT.from(Students)); // Ejecutamos la consulta
+    console.log(result); // Imprimimos los resultados en la consola
+    return result;
+  });
+
+
+  srv.on('READ', 'GetStudent', async (req) => {
+    console.log('GET request received for GetStudent');  // Log para asegurarte de que la solicitud se recibe
+    const {SELECT} = cds;
+    const aFilter = req.query.SELECT?.where;
+  
+    if (aFilter) {
+      console.log('Filter applied:', aFilter);  // Log de los filtros
+      return await SELECT.from(Students).where(aFilter);
+    }
+    return await SELECT.from(Students);
+  });
+
+  srv.after("READ", "GetStudent", data => {
+    return data.map(d => {
+      //d.first_name = d.first_name + " " + d.last_name;
+      return d;
+    });
+  });
+
+  srv.on("CREATE", "UpdateStudent", async (req, res) => {
+    let firstName = req.data.first_name;
+    let lastName = req.data.last_name;
+    let returnData = await cds
+      .transaction(req)
+      .run([
+        UPDATE(Students)
+          .set({
+            first_name: firstName
+          })
+          .where({ first_name: "Mr. " + firstName }),
+        UPDATE(Students)
+          .set({
+            last_name: "randomLast"
+          })
+          .where({ last_name: lastName })
+      ])
+      .then((resolve, reject) => {
+        console.log("resolve:", resolve);
+        console.log("reject:", reject);
+
+        if (typeof resolve !== "undefined") {
+          return req.data;
+        } else {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        req.error(500, "Error in Updating Record");
+      });
+    console.log("Before End", returnData);
+    return returnData;
+  });
+
+  srv.on("CREATE", "InsertStudent", async (req, res) => {
+    let returnData = await cds
+      .transaction(req)
+      .run(
+        INSERT.into(Students).entries({
+          email: req.data.email,
+          first_name: req.data.first_name,
+          last_name: req.data.last_name,
+          date_sign_up: req.data.date_sign_up
+        })
+      )
+      .then((resolve, reject) => {
+        console.log("resolve:", resolve);
+        console.log("reject:", reject);
+
+        if (typeof resolve !== "undefined") {
+          return req.data;
+        } else {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        req.error(500, "Error in Updating Record");
+      });
+    console.log("Before End", returnData);
+    return returnData;
+  });
+
+  srv.on("CREATE", "DeleteStudent", async (req, res) => {
+    let returnData = await cds
+      .transaction(req)
+      .run(
+        DELETE.from(Students).where({
+          email: req.data.email
+        })
+      )
+      .then((resolve, reject) => {
+        console.log("resolve:", resolve);
+        console.log("reject:", reject);
+
+        if (typeof resolve !== "undefined") {
+          return req.data;
+        } else {
+          req.error(409, "Record Not Found");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        req.error(500, "Error in Updating Record");
+      });
+    console.log("Before End", returnData);
+    return returnData;
+  });
+};
